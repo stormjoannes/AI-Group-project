673f894c40b0120502710bcb7d231464704ebbf2
DragonKiller952 <qjdegroot@gamil.com>
2020-03-30 15:48:13 +0200

    products naar csv converter

--- MongoDB products to CSV.py
+++ MongoDB products to CSV.py
@@ -10,41 +10,35 @@ mongo_client = MongoClient('localhost', 27017)
 # create new database and collection instance
 db = mongo_client.huwebshop
 col = db.products
-
+#
 # make an API call to the MongoDB server
-cursor = col.find()
+cursor = col.find({}, {'_id': 1, 'name': 1, 'brand': 1, 'properties.soort': 1, 'category': 1, 'sub_category': 1,
+                       'sub_sub_category': 1, 'properties.doelgroep': 1, 'price.mrsp': 1, 'price.discount': 1, 'price.selling_price': 1,
+                       'properties.discount': 1, 'description': 1})
 
 # extract the list of documents from cursor obj
 mongo_docs = list(cursor)
 
 # restrict the number of docs to export
 print ("total docs:", len(mongo_docs))
+for i in mongo_docs:
+    maindict = dict(i)
+    for j in maindict:
+        current = i.get(j)
+        if type(current) is dict:
+            del i[j]
+            for k in current:
+                new = current.get(k)
+                i[(j+'.'+k)] = new
 
 # create an empty DataFrame for storing documents
-docs = pandas.DataFrame(columns=[])
+docs = pandas.DataFrame(list(mongo_docs))
+docs = docs[['_id', 'name', 'brand', 'properties.soort', 'category', 'sub_category', 'sub_sub_category', 'properties.doelgroep',
+             'price.mrsp', 'price.discount', 'price.selling_price', 'properties.discount', 'description']]
+docs.head()
+docs = docs.rename(columns={'_id': 'id', 'properties.soort': 'type', 'sub_category': 'subcategory',
+                            'sub_sub_category': 'subsubcategory', 'properties.doelgroep': 'targetaudience',
+                            'price.mrsp': 'mrsp', 'price.discount': 'discount', 'price.selling_price': 'sellingprice',
+                            'properties.discount': 'deal'})
 
-# iterate over the list of MongoDB dict documents
-for num, doc in enumerate(mongo_docs):
-
-    # convert ObjectId() to str
-    doc["_id"] = str(doc["_id"])
-
-    # get document _id from dict
-    doc_id = doc["_id"]
-
-    # create a Series obj from the MongoDB dict
-    series_obj = pandas.Series( doc, name=doc_id )
-
-    # append the MongoDB Series obj to the DataFrame obj
-    docs = docs.append(series_obj)
-
-    # only print every 10th document
-    if num % 10 == 0:
-        print('\r' + str(num), end='')
-
-# export MongoDB documents to a CSV file
-docs.to_csv("products.csv", ",") # CSV delimited by commas
-
-# export MongoDB documents to CSV
-csv_export = docs.to_csv(sep=",") # CSV delimited by commas
-print ("\nCSV data:", csv_export)
+docs.to_csv('products.csv', index=False)
