c745f25ec0294e03e50b30d06dcc3d2ff72c842b
BKBetz <brandonbetz@live.nl>
2020-04-08 00:56:35 +0200

    refactored last files

--- backend/CatagoryConversion.py
+++ backend/CatagoryConversion.py
@@ -4,67 +4,86 @@ invoer = 'DataProductsPerUser'
 uitvoer = 'CategorysPerUser'
 alluitvoer = 'AllUsedCategorys'
 
+print("Getting DataProductsPerUser...")
 inv = open(invoer, 'r')
 uit = open(uitvoer, 'w')
 alluit = open(alluitvoer, 'w')
 
-print("Getting DataProductsPerUser...")
 
-# Haalt de data van elk bekeken product per profiel uit het .txt bestand.
+def get_data():
+    # Haalt de data van elk bekeken product per profiel uit het .txt bestand.
 
-read = inv.readlines()
-data = []
-for i in range(len(read)):
-    current_data = eval(read[i])
-    data.append(current_data)
+    read = inv.readlines()
+    data = []
+    for i in range(len(read)):
+        current_data = eval(read[i])
+        data.append(current_data)
+
+    return data
+
 
 print("Converting data...")
 
-# Splitst per profiel alle categorys, subcategorys, subsubcategorys en namen, en zet deze in aparte lijsten per profiel.
-# Zet daarnaast al de gespliteste code van alle profielen ook in een lijst, waaruit later de views per category worden berekend.
 
-inv.close()
-allcats = []
-allsubcats = []
-allsubsubcats = []
-allnames = []
-profcats = []
-for i in range(len(data)):
-    category = []
-    subcategory = []
-    subsubcategory = []
-    for j in range(len(data[i][1])):
-        category.append(data[i][1][j][0])
-        allcats.append(data[i][1][j][0])
-        subcategory.append(data[i][1][j][1])
-        allsubcats.append(data[i][1][j][1])
-        subsubcategory.append(data[i][1][j][2])
-        allsubsubcats.append(data[i][1][j][2])
-        allnames.append(data[i][1][j][3])
+def split_per_profile():
+    # Splitst per profiel alle categorys, subcategorys, subsubcategorys en namen, en zet deze in aparte lijsten per profiel.
+    # Zet daarnaast al de gespliteste code van alle profielen ook in een lijst, waaruit later de views per category worden berekend.
 
-    profcats.append(
-        [data[i][0], ['categorys', category], ['subcategorys', subcategory], ['subsubcategorys', subsubcategory]])
+    data = get_data()
+    profcats = []
+    allcats = []
+    allsubcats = []
+    allsubsubcats = []
+    allnames = []
 
-totallcats = [['categorys', allcats], ['subcategorys', allsubcats], ['subsubcategorys', allsubsubcats],
-              ['names', allnames]]
+    for i in range(len(data)):
+        category = []
+        subcategory = []
+        subsubcategory = []
+        for j in range(len(data[i][1])):
+            category.append(data[i][1][j][0])
+            allcats.append(data[i][1][j][0])
+            subcategory.append(data[i][1][j][1])
+            allsubcats.append(data[i][1][j][1])
+            subsubcategory.append(data[i][1][j][2])
+            allsubsubcats.append(data[i][1][j][2])
+            allnames.append(data[i][1][j][3])
+
+        profcats.append(
+            [data[i][0], ['categorys', category], ['subcategorys', subcategory], ['subsubcategorys', subsubcategory]])
+
+    totallcats = [['categorys', allcats], ['subcategorys', allsubcats], ['subsubcategorys', allsubsubcats],
+                    ['names', allnames]]
+
+    return profcats, totallcats
+
 
 print("Exporting data into CategorysPerUser and AllUsedCategorys...")
 
-# De data van categorys per profiel worden uit een list in een .txt bestand gezet.
 
-for i in range(len(profcats)):
-    print("\r{} of {} in file...".format(i + 1, len(profcats)), end="")
-    uit.write('{}\n'.format(profcats[i]))
+def write_to_files():
+    # De data van categorys per profiel worden uit een list in een .txt bestand gezet.
+    data = split_per_profile()
+    profcats = data[0]
+    totallcats = data[1]
 
-# De data van categorys van alle profielen wordt uit een list in een .txt bestand gezet.
+    for i in range(len(profcats)):
+        print("\r{} of {} in file...".format(i + 1, len(profcats)), end="")
+        uit.write('{}\n'.format(profcats[i]))
 
-for i in range(len(totallcats)):
-    alluit.write('{}\n'.format(totallcats[i]))
+    # De data van categorys van alle profielen wordt uit een list in een .txt bestand gezet.
+
+    for i in range(len(totallcats)):
+        alluit.write('{}\n'.format(totallcats[i]))
+
+    print("\nPrinted {} items!".format(len(profcats)))
+
+
+write_to_files()
 
 uit.close()
 alluit.close()
-print("\nPrinted {} items!".format(len(profcats)))
-
+inv.close()
 # Start het eerstvolgende bestand.
 
 exec(open('ProductviewsToDatabase.py').read())
--- backend/RecommendPerProduct_names.py
+++ backend/RecommendPerProduct_names.py
@@ -6,6 +6,8 @@ cur = db[0]
 conn = db[1]
 tijd = time.time()
 print("\n### RecommendPerProduct_names.py ###\n")
+
+
 def vergelijking(naam):
     naam = naam[0]
     if naam:
@@ -79,154 +81,193 @@ def vergelijking(naam):
         #             "subsubcatrecommend = '{}', namerecommend = '{}' "
         #             "WHERE name = '{}'".format(recs[0], recs[1], recs[2], recs[3], naam))
 
-print("Setting up tables...")
-# Maakt tabel met data van views per product uit tabel products.
-cur.execute("DROP TABLE IF EXISTS valuesproducts")
-cur.execute("CREATE TABLE valuesproducts AS (select id, name, targetaudience, brand, category, subcategory, "
-            "subsubcategory, categoryviews, subcategoryviews, subsubcategoryviews, productviews from products)")
-# Zet kolommen voor recommendations op basis van category in tabel products.
-cur.execute("ALTER TABLE products DROP COLUMN IF EXISTS catrecommend")
-cur.execute("ALTER TABLE products DROP COLUMN IF EXISTS subcatrecommend")
-cur.execute("ALTER TABLE products DROP COLUMN IF EXISTS subsubcatrecommend")
-cur.execute("ALTER TABLE products DROP COLUMN IF EXISTS namerecommend")
-cur.execute("ALTER TABLE products ADD catrecommend varchar")
-cur.execute("ALTER TABLE products ADD subcatrecommend varchar")
-cur.execute("ALTER TABLE products ADD subsubcatrecommend varchar")
-cur.execute("ALTER TABLE products ADD namerecommend varchar")
-conn.commit()
-print("Getting data...")
-# Haalt alle categorys op en zorgt ervoor dat er maar 1 van elk in de lijst staat.
-cur.execute("select category from products")
-categorys = list(set(cur.fetchall()))
-# Haalt alle subcategorys op en zorgt ervoor dat er maar 1 van elk in de lijst staat.
-cur.execute("select subcategory from products")
-subcategorys = list(set(cur.fetchall()))
-# Haalt alle subsubcategorys op en zorgt ervoor dat er maar 1 van elk in de lijst staat.
-cur.execute("select subsubcategory from products")
-subsubcategorys = list(set(cur.fetchall()))
-print("Calculating category recommendations...")
-# Haalt recommendations per category op aan de hand van categoryviews,
-# en zet deze in products bij de juiste categorys.
-# Daarnaast zet hij in de lege velden het meest overal bekeken product op basis van meest bekeken category.
-conn.commit()
-catrecs = []
-for i in range(len(categorys)):
-    if categorys[i][0]:
-        if '[' not in categorys[i][0]:
-            name = categorys[i][0]
+
+def setup_tables():
+    print("Setting up tables...")
+    # Maakt tabel met data van views per product uit tabel products.
+    cur.execute("DROP TABLE IF EXISTS valuesproducts")
+    cur.execute("CREATE TABLE valuesproducts AS (select id, name, targetaudience, brand, category, subcategory, "
+                "subsubcategory, categoryviews, subcategoryviews, subsubcategoryviews, productviews from products)")
+    # Zet kolommen voor recommendations op basis van category in tabel products.
+    cur.execute("ALTER TABLE products DROP COLUMN IF EXISTS catrecommend")
+    cur.execute("ALTER TABLE products DROP COLUMN IF EXISTS subcatrecommend")
+    cur.execute("ALTER TABLE products DROP COLUMN IF EXISTS subsubcatrecommend")
+    cur.execute("ALTER TABLE products DROP COLUMN IF EXISTS namerecommend")
+    cur.execute("ALTER TABLE products ADD catrecommend varchar")
+    cur.execute("ALTER TABLE products ADD subcatrecommend varchar")
+    cur.execute("ALTER TABLE products ADD subsubcatrecommend varchar")
+    cur.execute("ALTER TABLE products ADD namerecommend varchar")
+    conn.commit()
+    print("Getting data...")
+    # Haalt alle categorys op en zorgt ervoor dat er maar 1 van elk in de lijst staat.
+    cur.execute("select category from products")
+    categorys = list(set(cur.fetchall()))
+    # Haalt alle subcategorys op en zorgt ervoor dat er maar 1 van elk in de lijst staat.
+    cur.execute("select subcategory from products")
+    subcategorys = list(set(cur.fetchall()))
+    # Haalt alle subsubcategorys op en zorgt ervoor dat er maar 1 van elk in de lijst staat.
+    cur.execute("select subsubcategory from products")
+    subsubcategorys = list(set(cur.fetchall()))
+
+    conn.commit()
+
+    return categorys, subcategorys, subsubcategorys
+
+
+def catrecordings():
+    print("Calculating category recommendations...")
+    # Haalt recommendations per category op aan de hand van categoryviews,
+    # en zet deze in products bij de juiste categorys.
+    # Daarnaast zet hij in de lege velden het meest overal bekeken product op basis van meest bekeken category.
+    tables = setup_tables()
+    categorys =tables[0]
+    catrecs = []
+    for i in range(len(categorys)):
+        if categorys[i][0]:
+            if '[' not in categorys[i][0]:
+                name = categorys[i][0]
+                if "'" in name:
+                    currentcat = name.split("'")
+                    name = "''".join(map(str, currentcat))
+                cur.execute("select id from valuesproducts where categoryviews is not null and "
+                            "productviews is not null and category = '{}'"
+                            "order by categoryviews desc, subcategoryviews desc, "
+                            "subsubcategoryviews desc, productviews desc "
+                            "limit 2".format(name))
+                fetch = cur.fetchall()
+                if len(fetch) == 1:
+                    catrecs.append([name, [fetch[0], fetch[0]]])
+                else:
+                    catrecs.append([name, fetch])
+    for i in range(len(catrecs)):
+        if catrecs[i][1]:
+            if catrecs[i][0] != 'None':
+                for j in range(len(catrecs[i][1])):
+                    cur.execute("UPDATE products SET catrecommend = '{}' "
+                                "WHERE category = '{}' "
+                                "and catrecommend is null "
+                                "and id != '{}'".format(catrecs[i][1][j][0], catrecs[i][0], catrecs[i][1][j][0]))
+    conn.commit()
+
+
+def subcatrecordings():
+
+    print("Calculating subcategory recommendations...")
+    # Haalt recommendations per subcategory op aan de hand van subcategoryviews,
+    # en zet deze in products bij de juiste subcategorys.
+    # Daarnaast zet hij in de lege velden het meest overal bekeken product op basis van meest bekeken subcategory.
+
+    tables = setup_tables()
+    subcategorys = tables[1]
+    subcatrecs = []
+    for i in range(len(subcategorys)):
+        name = subcategorys[i][0]
+        if subcategorys[i][0]:
             if "'" in name:
-                currentcat = name.split("'")
-                name = "''".join(map(str, currentcat))
-            cur.execute("select id from valuesproducts where categoryviews is not null and "
-                        "productviews is not null and category = '{}'"
+                currentsubcat = name.split("'")
+                name = "''".join(map(str, currentsubcat))
+            cur.execute("select id from valuesproducts where subsubcategoryviews is not null and "
+                        "productviews is not null and subsubcategory = '{}'"
                         "order by categoryviews desc, subcategoryviews desc, "
                         "subsubcategoryviews desc, productviews desc "
-                        "limit 2".format(name))
+                        "limit 3".format(name))
             fetch = cur.fetchall()
             if len(fetch) == 1:
-                catrecs.append([name, [fetch[0], fetch[0]]])
+                subcatrecs.append([name, [fetch[0], fetch[0], fetch[0]]])
+            elif len(fetch) == 2:
+                subcatrecs.append([name, [fetch[0], fetch[1], fetch[1]]])
             else:
-                catrecs.append([name, fetch])
-for i in range(len(catrecs)):
-    if catrecs[i][1]:
-        if catrecs[i][0] != 'None':
-            for j in range(len(catrecs[i][1])):
-                cur.execute("UPDATE products SET catrecommend = '{}' "
-                            "WHERE category = '{}' "
-                            "and catrecommend is null "
-                            "and id != '{}'".format(catrecs[i][1][j][0], catrecs[i][0], catrecs[i][1][j][0]))
-print("Calculating subcategory recommendations...")
-# Haalt recommendations per subcategory op aan de hand van subcategoryviews,
-# en zet deze in products bij de juiste subcategorys.
-# Daarnaast zet hij in de lege velden het meest overal bekeken product op basis van meest bekeken subcategory.
-subcatrecs = []
-for i in range(len(subcategorys)):
-    name = subcategorys[i][0]
-    if subcategorys[i][0]:
-        if "'" in name:
-            currentsubcat = name.split("'")
-            name = "''".join(map(str, currentsubcat))
-        cur.execute("select id from valuesproducts where subsubcategoryviews is not null and "
-                    "productviews is not null and subsubcategory = '{}'"
-                    "order by categoryviews desc, subcategoryviews desc, "
-                    "subsubcategoryviews desc, productviews desc "
-                    "limit 3".format(name))
-        fetch = cur.fetchall()
-        if len(fetch) == 1:
-            subcatrecs.append([name, [fetch[0], fetch[0], fetch[0]]])
-        elif len(fetch) == 2:
-            subcatrecs.append([name, [fetch[0], fetch[1], fetch[1]]])
-        else:
-            subcatrecs.append([name, fetch])
-for i in range(len(subcatrecs)):
-    if subcatrecs[i][1]:
-        if subcatrecs[i][0] != 'None':
-            for j in range(len(subcatrecs[i][1])):
-                cur.execute("UPDATE products SET subcatrecommend = '{}' "
-                            "WHERE subcategory = '{}' "
-                            "and catrecommend != '{}' "
-                            "and subcatrecommend is null "
-                            "and id != '{}'".format(subcatrecs[i][1][j][0], subcatrecs[i][0],
-                                                    subcatrecs[i][1][j][0], subcatrecs[i][1][j][0]))
-conn.commit()
-print("Calculating subsubcategory recommendations...")
-# Haalt recommendations per subsubcategory op aan de hand van subsubcategoryviews,
-# en zet deze in products bij de juiste subsubcategorys.
-# Daarnaast zet hij in de lege velden het meest overal bekeken product op basis van meest bekeken subsubcategory.
-subsubcatrecs = []
-for i in range(len(subsubcategorys)):
-    if subsubcategorys[i][0]:
-        name = subsubcategorys[i][0]
-        if "'" in name:
-            currentsubsubcat = name.split("'")
-            name = "''".join(map(str, currentsubsubcat))
-        cur.execute("select id from valuesproducts where subsubcategoryviews is not null and "
-                    "productviews is not null and subsubcategory = '{}'"
-                    "order by categoryviews desc, subcategoryviews desc, "
-                    "subsubcategoryviews desc, productviews desc "
-                    "limit 4".format(name))
-        fetch = cur.fetchall()
-        if len(fetch) == 1:
-            subsubcatrecs.append([name, [fetch[0], fetch[0], fetch[0], fetch[0]]])
-        elif len(fetch) == 2:
-            subsubcatrecs.append([name, [fetch[0], fetch[1], fetch[1], fetch[1]]])
-        elif len(fetch) == 2:
-            subsubcatrecs.append([name, [fetch[0], fetch[1], fetch[2], fetch[2]]])
-        else:
-            subsubcatrecs.append([name, fetch])
-for i in range(len(subsubcatrecs)):
-    if subsubcatrecs[i][1]:
-        if subsubcatrecs[i][0] != 'None':
-            for j in range(len(subsubcatrecs[i][1])):
-                cur.execute("UPDATE products SET subsubcatrecommend = '{}' "
-                            "WHERE subsubcategory = '{}' "
-                            "and subcatrecommend != '{}' "
-                            "and catrecommend != '{}' "
-                            "and subsubcatrecommend is null "
-                            "and id != '{}'".format(subsubcatrecs[i][1][j][0], subsubcatrecs[i][0],
-                                                    subsubcatrecs[i][1][j][0], subsubcatrecs[i][1][j][0],
-                                                    subsubcatrecs[i][1][j][0]))
-print("Calculating name recommendations...")
-cur.execute("select name FROM products")
-namen = cur.fetchall()
-count = 0
-conn.commit()
-for naam in namen:
-    count += 1
-    print('\r{} of {}'.format(count, len(namen)), end='')
-    vergelijking(naam)
+                subcatrecs.append([name, fetch])
+    for i in range(len(subcatrecs)):
+        if subcatrecs[i][1]:
+            if subcatrecs[i][0] != 'None':
+                for j in range(len(subcatrecs[i][1])):
+                    cur.execute("UPDATE products SET subcatrecommend = '{}' "
+                                "WHERE subcategory = '{}' "
+                                "and catrecommend != '{}' "
+                                "and subcatrecommend is null "
+                                "and id != '{}'".format(subcatrecs[i][1][j][0], subcatrecs[i][0],
+                                                        subcatrecs[i][1][j][0], subcatrecs[i][1][j][0]))
     conn.commit()
-print("\nCreating table with recommendations per product...")
-# Maakt tabel met recommendations per product uit tabel products.
-cur.execute("DROP TABLE IF EXISTS product_recommendations")
-cur.execute("CREATE TABLE product_recommendations AS (select id, catrecommend, subcatrecommend, subsubcatrecommend, "
-            "namerecommend from products)")
-print("Recommendations created for products!")
-conn.commit()
-cur.close()
-conn.close()
-eind = time.time()
-print('tijd:')
-print(eind-tijd)
+
+
+def subsubcatrecordings():
+    print("Calculating subsubcategory recommendations...")
+    # Haalt recommendations per subsubcategory op aan de hand van subsubcategoryviews,
+    # en zet deze in products bij de juiste subsubcategorys.
+    # Daarnaast zet hij in de lege velden het meest overal bekeken product op basis van meest bekeken subsubcategory.
+    tables = setup_tables()
+    subsubcategorys = tables[2]
+    subsubcatrecs = []
+    for i in range(len(subsubcategorys)):
+        if subsubcategorys[i][0]:
+            name = subsubcategorys[i][0]
+            if "'" in name:
+                currentsubsubcat = name.split("'")
+                name = "''".join(map(str, currentsubsubcat))
+            cur.execute("select id from valuesproducts where subsubcategoryviews is not null and "
+                        "productviews is not null and subsubcategory = '{}'"
+                        "order by categoryviews desc, subcategoryviews desc, "
+                        "subsubcategoryviews desc, productviews desc "
+                        "limit 4".format(name))
+            fetch = cur.fetchall()
+            if len(fetch) == 1:
+                subsubcatrecs.append([name, [fetch[0], fetch[0], fetch[0], fetch[0]]])
+            elif len(fetch) == 2:
+                subsubcatrecs.append([name, [fetch[0], fetch[1], fetch[1], fetch[1]]])
+            elif len(fetch) == 2:
+                subsubcatrecs.append([name, [fetch[0], fetch[1], fetch[2], fetch[2]]])
+            else:
+                subsubcatrecs.append([name, fetch])
+    for i in range(len(subsubcatrecs)):
+        if subsubcatrecs[i][1]:
+            if subsubcatrecs[i][0] != 'None':
+                for j in range(len(subsubcatrecs[i][1])):
+                    cur.execute("UPDATE products SET subsubcatrecommend = '{}' "
+                                "WHERE subsubcategory = '{}' "
+                                "and subcatrecommend != '{}' "
+                                "and catrecommend != '{}' "
+                                "and subsubcatrecommend is null "
+                                "and id != '{}'".format(subsubcatrecs[i][1][j][0], subsubcatrecs[i][0],
+                                                        subsubcatrecs[i][1][j][0], subsubcatrecs[i][1][j][0],
+                                                        subsubcatrecs[i][1][j][0]))
+
+    conn.commit()
+
+
+def namerecordings():
+    print("Calculating name recommendations...")
+    cur.execute("select name FROM products")
+    namen = cur.fetchall()
+    count = 0
+    conn.commit()
+    for naam in namen:
+        count += 1
+        print('\r{} of {}'.format(count, len(namen)), end='')
+        vergelijking(naam)
+        conn.commit()
+    print("\nCreating table with recommendations per product...")
+    # Maakt tabel met recommendations per product uit tabel products.
+    cur.execute("DROP TABLE IF EXISTS product_recommendations")
+    cur.execute("CREATE TABLE product_recommendations AS (select id, catrecommend, subcatrecommend, subsubcatrecommend, "
+                "namerecommend from products)")
+    print("Recommendations created for products!")
+    eind = time.time()
+    print('tijd:')
+    print(eind-tijd)
+    conn.commit()
+
+
+def create_recordings():
+    catrecordings()
+    subcatrecordings()
+    subsubcatrecordings()
+    namerecordings()
+
+    cur.close()
+    conn.close()
+
+
+create_recordings()
 
 exec(open('gender-discount_per_user.py').read())
