8ad8bf53b6b31471c913bd53ad913c04a7060d9a
Storm Joannes <storm.joannes@gmail.com>
2020-04-08 15:07:15 +0200

    storm

--- backend/gender-discount_per_user.py
+++ backend/gender-discount_per_user.py
@@ -19,7 +19,7 @@ def base():
     all = all_inf()
     all_profid = all[0]
     all_prodid = all[1]
-    for i in range(0, len(all_profid)):
+    for i in range(820000, len(all_profid)):
         print("\rRendering profile recommendations: {} from 2081649....".format(i), end='')
         executer = """select deal, targetaudience from products where id = %s or id = %s or id = %s;"""
         cur.execute(executer, (all_prodid[i][0], all_prodid[i][1], all_prodid[i][2],))
@@ -37,6 +37,7 @@ def base():
         rand_choice = recommended(targ_prod, best_deal)
         random.shuffle(rand_choice)
         all_rand_choice = rand_choice[0:4]
+        print(all_rand_choice)
         cur.execute("INSERT INTO all_prof_rec (id_, "
                     "recommendation_1, "
                     "recommendation_2, "
@@ -63,6 +64,11 @@ def recommended(targ_prod, best_deal):
         exe = """select id from products where targetaudience LIKE %s LIMIT 10"""
         cur.execute(exe, (targ_prod,))
         all_rec = cur.fetchall()
+        if len(all_rec) < 4:
+            exe = """select id from products where deal LIKE %s LIMIT 10"""
+            cur.execute(exe, (best_deal,))
+            all_rec = cur.fetchall()
+
     return all_rec
 
 def most_common(list):
